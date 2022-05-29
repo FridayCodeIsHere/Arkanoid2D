2b40a9df26f6658091634347377545c39443e394
esuriens <walasek.jan@seznam.cz>
2022-05-29 21:08:17 +0200

    normalize speed of ball

--- ArkanoidProject2022/Assets/Scripts/Ball/BallMovement.cs
+++ ArkanoidProject2022/Assets/Scripts/Ball/BallMovement.cs
@@ -10,7 +10,7 @@ namespace ArkanoidProj
         [SerializeField] private GameObject _lineEffect;
         private Rigidbody2D _rigidbody;
         private bool _isActive = false;
-        private const float Force = 400f;
+        private const float Force = 10f;
         private float _lastPositionX;
 
         private void OnEnable()
@@ -38,16 +38,15 @@ namespace ArkanoidProj
                 Debug.Log($"IsActive: {_isActive}");
                 transform.SetParent(null);
                 _rigidbody.bodyType = RigidbodyType2D.Dynamic;
-                AddForce();
+                AddForce(0, -1); //fall down at the start
             }
 
             
         }
 
-        public void AddForce(float direction = 0f)
+        public void AddForce(float dirX = 0f, float dirY = 1f)
         {
-            _rigidbody.velocity = Vector2.zero;
-            _rigidbody.AddForce(new Vector2(direction * (Force / 4), Force));
+            _rigidbody.velocity = new Vector2(dirX * 4, dirY).normalized * Force;
         }
     }
 }
